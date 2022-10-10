8acc798c6e7d4bdb10908f5db5a0278079674360
naoki <naokipgt@gmail.com>
2022-10-10 15:38:20 +0900

    routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <Tutorial/>
+  <div>
+    <NuxtLink to="/fun">test page</NuxtLink>
+
+    <main>
+      <h1>Home page</h1>
+      <NuxtLink to="/about">
+        About (internal link that belongs to the Nuxt App)
+      </NuxtLink>
+      <a href="https://nuxtjs.org">External Link to another page</a>
+    </main>
+  </div>
 </template>
 
 <script>
