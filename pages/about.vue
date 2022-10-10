7c300485a514c341f401474d6b756a5f71c52a91
naoki <naokipgt@gmail.com>
2022-10-10 16:02:33 +0900

    lazy import

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
     about me
+    <button @click="action">
+      show
+    </button>
+    <LazyTest v-if="show"></LazyTest>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    action() {
+      this.show = !this.show
+    }
+  }
+}
+</script>
